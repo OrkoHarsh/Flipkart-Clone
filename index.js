37b565e9a3f1d049af5db0356f4441c89eb68529
OrkoHarsh <harsh_sharma69@outlook.com>
2024-02-24 15:31:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { v4 as uuid } from 'uuid';
+import { fileURLToPath } from 'url'; // Import fileURLToPath
+import path from 'path';
+
+import Connection from './database/db.js';
+import DefaultData from './default.js';
+import Routes from './routes/route.js';
+
+dotenv.config();
+const app = express();
+
+const PORT = 8000;
+
+// Get current directory using import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+Connection(username, password);
+
+app.get("/",(req,res)=>{
+    app.use(express.static(path.resolve(__dirname,"client","build")));
+    res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+})
+
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+DefaultData();
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/', Routes);
+
+export let paytmMerchantkey = process.env.PAYTM_MERCHANT_KEY;
+export let paytmParams = {};
+// paytmParams['MID'] = process.env.PAYTM_MID;
+paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE;
+paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID;
+paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID;
+paytmParams['ORDER_ID'] = uuid();
+paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID;
+paytmParams['TXN_AMOUNT'] = '100';
+paytmParams['CALLBACK_URL'] = `http://localhost:8000/callback`;
+paytmParams['EMAIL'] = 'hrsh0412@gmail.com';
+paytmParams['MOBILE_NO'] = '1234567852';
